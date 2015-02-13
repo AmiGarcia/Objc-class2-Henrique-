475b9bb8a876f567a398ecfbf444985baf284675
Henrique Valcanaia <valcanaia@Henriques-MacBook-Pro.local>
2015-02-13 14:20:11 -0200

    Initial Commit

---
+++ Objc-class2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objc-class2
+//
+//  Created by Henrique Valcanaia on 2/13/15.
+//  Copyright (c) 2015 Henrique Valcanaia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
